826c2308afcef07c800546161671b9fcb54f1f4b
Luke <luke@Lukes-MacBook-Air.local>
2025-04-13 10:21:16 +0700

    update

--- inventory/models.py
+++ inventory/models.py
@@ -79,13 +79,16 @@ class DeviceGroup(models.Model):
 class Device(models.Model):
     device_name = models.CharField(max_length=200)
     device_ip = models.GenericIPAddressField(unique=True)
-    device_province = models.ForeignKey('DeviceProvince', on_delete=models.PROTECT)
+    device_province = models.ForeignKey(
+        'DeviceProvince', on_delete=models.PROTECT)
     device_branch = models.ForeignKey('DeviceBranch', on_delete=models.PROTECT)
     device_type = models.ForeignKey('DeviceType', on_delete=models.PROTECT)
-    device_category = models.ForeignKey('DeviceCategory', on_delete=models.PROTECT)
+    device_category = models.ForeignKey(
+        'DeviceCategory', on_delete=models.PROTECT)
     device_vendor = models.ForeignKey('DeviceVendor', on_delete=models.PROTECT)
     device_os = models.ForeignKey('DeviceOS', on_delete=models.PROTECT)
-    device_group = models.ForeignKey('DeviceGroup', on_delete=models.PROTECT, null=True, blank=True)
+    device_group = models.ForeignKey(
+        'DeviceGroup', on_delete=models.PROTECT, null=True, blank=True)
     device_firmware = models.CharField(max_length=200, blank=True)
     device_status = models.CharField(max_length=200, blank=True)
     device_stack = models.BooleanField(default=False)
@@ -101,7 +104,8 @@ class DeviceManagement(models.Model):
     """Model definition for DeviceManagement."""
 
     device_ip = models.ForeignKey('Device', on_delete=models.CASCADE)
-    device_serial_number = models.CharField(max_length=100, blank=True, unique=True)
+    device_serial_number = models.CharField(
+        max_length=100, blank=True, unique=True)
     start_ma_date = models.DateField(blank=True, null=True)
     end_ma_date = models.DateField(blank=True, null=True)
     start_license_date = models.DateField(blank=True, null=True)
@@ -114,17 +118,18 @@ class DeviceManagement(models.Model):
     def __str__(self):
         """Unicode representation of DeviceManagement."""
         return str(self.device_serial_number)
-    
+
 
 class DeviceRackLayout(models.Model):
     """Model definition for DeviceRackLayout."""
-    
+
     device_ip = models.ForeignKey('Device', on_delete=models.CASCADE)
-    device_serial_number = models.OneToOneField('DeviceManagement', on_delete=models.CASCADE)
+    device_serial_number = models.OneToOneField(
+        'DeviceManagement', on_delete=models.CASCADE)
     device_rack_name = models.CharField(max_length=200, blank=True)
     device_rack_unit = models.CharField(max_length=200, blank=True)
     user_created = models.CharField(max_length=100, blank=True)
-    
+
     def __str__(self):
         """Unicode representation of DeviceRackLayout."""
         return str(self.device_ip)
@@ -138,7 +143,7 @@ class DeviceFirmware(models.Model):
     description = models.CharField(max_length=200, blank=True)
     creation_time = models.DateTimeField(auto_now=True)
     user_created = models.CharField(max_length=100, blank=True)
-    
+
     def __str__(self):
         """Unicode representation of DeviceFirmware."""
         return str(self.firmware)
